5b49b543d8047bfc3a988892053cfd0db5f57cc2
Bibek <bibeksilwal.32@gmail.com>
2025-03-18 03:45:11 +0000

    Added the function to add new div using js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let library = [];
 
+
 function Book(id,title, author, pages, read){
     this.id = id;
     this.title = title;
@@ -13,5 +14,45 @@ function storeBook(title,author,pages,read){
     library.push(book1);
 }
 
-storeBook("Bibek", "Silwal", 28, true);
-console.log(library);
+function createBookDiv(title, author, pages, read){
+
+    let parentDiv = document.createElement("div");
+    parentDiv.className = "book";
+
+    let titleSection = document.createElement("div");
+    titleSection.className = "title-section";
+
+    let bookTitle = document.createElement("h3");
+    bookTitle.textContent = title;
+
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = read
+
+    titleSection.appendChild(bookTitle);
+    titleSection.appendChild(checkbox);
+
+
+    let bookInfo = document.createElement("div");
+    bookInfo.className = "book-info";
+
+    let authorElem = document.createElement("p");
+    authorElem.textContent = author;
+
+    let pagesElem = document.createElement("p");
+    pagesElem.textContent = pages;
+
+    bookInfo.appendChild(authorElem);
+    bookInfo.appendChild(pagesElem);
+
+    parentDiv.appendChild(titleSection);
+    parentDiv.appendChild(bookInfo);
+
+    return parentDiv;
+
+}
+
+let container = document.querySelector(".books");
+
+let book1 = createBookDiv("Bibek", "Silwal", 20, true);
+container.appendChild(book1);
